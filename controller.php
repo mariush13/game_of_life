c37a3d92aa1d1a0bc50e56383a30a8c7227a5023
Krzysztof Moskalik <krzysztof.moskalik@fsi.pl>
2012-10-03 11:59:28 +0200

    first all working version

--- controller.php
+++ controller.php
@@ -2,29 +2,73 @@
 
 class GameOfLife {
 	
-	public function __construct($x, $y, $fill){
-		$this->x = $x;
-		$this->y = $y;
-		$this->fill = $fill;
+    public $matrix;
+    
+	public function __construct($xajax) {
 		$this->matrix = array();	
+		$this->xajax=$xajax;
+        $this->response = new xajaxResponse();
 	}
 	
-	public function generate(){
+	
+	public function generate($x, $y, $fill) {
+	    $this->x = $x;
+		$this->y = $y;
+		$this->fill = $fill;
 		for ($i=0; $i < $this->x; $i++){
 			for ($j=0; $j<$this->y; $j++){
 				$this->matrix[$i][$j] = (rand(1,100) <= $this->fill);
 			}
 		}
+		$this->saveToFile();
+		$this->response->assign('matrix','innerHTML',$this->render(20,20));
+	    return $this->response;
 	}
 	
-	public function render($width, $height) {
+	public function saveToFile() {
+	    $h = fopen('matrix.txt','w');
+	    fwrite($h,serialize($this->matrix));
+	    fclose($h);  
+	}
+	
+	public function openFromFile(){
+	    $this->matrix = unserialize(file_get_contents('matrix.txt'));
+	    $this->response->assign('matrix','innerHTML',$this->render(20,20));
+	    return $this->response;
+	    $this->x = sizeof($this->matrix);
+	    $this->y = sizeof($this->matrix[0]);  
+	}
+	
+	private function render($width, $height) {
+	    $this->x = sizeof($this->matrix);
+	    $this->y = sizeof($this->matrix[0]);  
 		$content = '';
 		for ($i=0; $i < $this->x; $i++){
 			for ($j=0; $j<$this->y; $j++){
-			$content .= '<div class="cell '.(($this->matrix[$i][$j])? 'true' : 'false').'" style="left: '.$i*$width.'px; top: '.$j*$height.'px; "></div>';	
+			$content .= '<div class="cell '.(($this->matrix[$i][$j])? 'true' : 'false').'" style="left: '.$i*$width.'px; top: '.$j*$height.'px; ">
+			
+			</div>';	
 			}
 		}		
 		return $content;
+	}
+	
+	private function getNeighbors($x, $y){
+	$neighbors = array(
+		((isset($this->matrix[$x-1][$y-1])) ? $this->matrix[$x-1][$y-1] : false),
+		((isset($this->matrix[$x][$y-1])) ? $this->matrix[$x][$y-1] : false),
+		((isset($this->matrix[$x+1][$y-1])) ? $this->matrix[$x+1][$y-1] : false),
+		((isset($this->matrix[$x-1][$y])) ? $this->matrix[$x-1][$y] : false),
+		((isset($this->matrix[$x+1][$y])) ? $this->matrix[$x+1][$y] : false),
+		((isset($this->matrix[$x-1][$y+1])) ? $this->matrix[$x-1][$y+1] : false),
+		((isset($this->matrix[$x][$y+1])) ? $this->matrix[$x][$y+1] : false),
+		((isset($this->matrix[$x+1][$y+1])) ? $this->matrix[$x+1][$y+1] : false)
+		);	
+		$count = 0;
+		foreach (array_values($neighbors) as $n){
+			if ($n){ $count++; }
+		}    
+		return $count;
 	}
 	
 	private function getState($x, $y){
@@ -43,33 +87,34 @@ class GameOfLife {
 			if ($n){ $count++; }
 		}
 		
+		// martwa komórka z 3 sąsiadami rodzi się
+		if (!$this->matrix[$x][$y] && $count == 3){
+		    return true;
+		}
 		
-		if ($this->matrix[$x][$y]){
-			if ($count==2 or $count == 3){
-				return true;
-			}else {
-				return false;
-			}
-		}else {
-			if ($count==3){
-				return true;
-			}else {
-				return false;
-			}
+		// żywa komórka z 2 lub 3 sąsiadami pozostaje przy życiu
+		if ($this->matrix[$x][$y] && in_array($count,array(2,3))){
+		    return true;
+		}
+		
+		// żywa komórka z mniej niż dwoma lub więcej niż 3 sąsiadami umiera
+		if ($this->matrix[$x][$y] && in_array($count, array(0,1,4,5,6,7,8))){
+		    return false;
 		}
 	}
 	
 	public function changeState(){
-		$this->newMatrix = array();
+	    $this->openFromFile();
 		for ($i=0; $i < $this->x; $i++){
 			for ($j=0; $j<$this->y; $j++){
-				$this->newMatrix[$i][$j] = $this->getState($i,$j);
+				$this->matrix[$i][$j] = $this->getState($i,$j);
 					
 			}	
 	
 		}
-		$this->matrix[$i][$j]= $this->newMatrix;
-		return $this->render(20,20);
+        $this->saveToFile();
+		$this->response->assign('matrix','innerHTML',$this->render(20,20));
+	    return $this->response;
 	}	
 	
 }
--- index.php
+++ index.php
@@ -1,37 +1,44 @@
 <?php 
-error_reporting(E_ALL);
+error_reporting( E_ALL & ~E_DEPRECATED); 
+ini_set( 'display_errors', 1);
+
 require'controller.php';
 include 'xajax/xajax_core/xajax.inc.php';
 
 $xajax = new xajax();
-//$xajax->setFlag('debug',true);
+$xajax->configure('debug',false);
+
 ?>
 <?php 
 
-$gol = new gameOfLife(5,5,20);
+$gol = new gameOfLife($xajax);
 
-$gol->generate();
 
-function changeState(){
-	global $gol;
-	
-	$objResponse = new xajaxResponse();
-	//$objResponse->alert('sad');
-	$objResponse->assign('matrix','innerHTML',$gol->changeState());
-	return $objResponse;
-}
-$xajax->registerFunction('changeState');
+$xajax->register(XAJAX_CALLABLE_OBJECT,$gol);
 $xajax->processRequest();
 
 ?>
 <html>
-<head>
-<link rel="stylesheet" type="text/css" href="global.css">
-<?php $xajax->printJavascript(); ?>
-</head>
-<body>
-<div id="matrix"></div>
-
-<button onclick="xajax_changeState();" value="Zmień" class="button"></button>
-</body>
+    <head>
+        <link rel="stylesheet" type="text/css" href="global.css">
+        <?php $xajax->printJavascript(); ?>
+        <script type="text/JavaScript">
+            function generate() {
+                x = document.getElementById('x').value;
+                y = document.getElementById('y').value;
+                fill = document.getElementById('fill').value;
+                xajax_GameOfLife.generate(x,y,fill);
+            }  
+        </script>
+    </head>
+    <body>
+        <div id="matrix"></div>
+        <div id="buttons">
+            <input type="text" id="x" value="20">
+            <input type="text" id="y" value="20">
+            <input type="text" id="fill" value="20">
+            <button onclick="generate();" class="button">Generuj</button>
+            <button onclick="xajax_GameOfLife.changeState();" class="button" id="c">Zmien</button>
+        </div>
+    </body>
 </html>
